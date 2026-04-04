f7973eecf9da90635484c57faf272a9e97e96dde
mardommah <reskyikky@gmail.com>
2026-04-04 15:42:21 +0800

    Implement Issue Analysis Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnalyticsController;
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,5 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [TicketController::class, 'index'])->name('dashboard');
+Route::get('/analytics', [AnalyticsController::class, 'index'])->name('analytics.index');
 Route::resource('tickets', TicketController::class);
